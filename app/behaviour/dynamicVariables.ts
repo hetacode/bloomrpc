96793383cdfc59be6d0dd14541f3e8d1ce2c628f
Seba G <sg@hetacode.pl>
2022-01-09 22:23:11 +0100

    Ability to replace all occurrence of the same variable

--- app/behaviour/dynamicVariables.ts
+++ app/behaviour/dynamicVariables.ts
@@ -1,21 +1,23 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid"
 
-const variableAction: { [variable: string]: (p: string) => string; } = {
-    "{{:guid}}": generateGuid
-    // TODO:
-    // :date now
-    // :rnd min max - random between
+const variableAction: { [variable: string]: (p: string) => string } = {
+  "{{:guid}}": generateGuid,
+  // TODO:
+  // :date now
+  // :rnd min max - random between
 }
 
 export const dynamicVariablesParser = (data: string) => {
-    let result = data
-    for (let [key, value] of Object.entries(variableAction)) {
-        result = result.replace(key, value(key))
+  let result = data
+  for (let [key, value] of Object.entries(variableAction)) {
+    let variableOccurencesCount = result.match(new RegExp(key, "g"))?.length ?? 0
+    for (let i = 0; i < variableOccurencesCount; i++) {
+      result = result.replace(key, value(key))
     }
-
-    return result
+  }
+  return result
 }
 
 function generateGuid(input: string) {
-    return uuidv4()
+  return uuidv4()
 }
