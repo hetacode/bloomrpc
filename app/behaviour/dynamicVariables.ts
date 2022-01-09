518f0dedaeac52b4facdfbcbcc0ce487a713486c
Seba G <sg@hetacode.pl>
2022-01-09 23:43:12 +0100

    Better variables parser - handling provided parameters
    + added :date variable

--- app/behaviour/dynamicVariables.ts
+++ app/behaviour/dynamicVariables.ts
@@ -1,9 +1,9 @@
 import { v4 as uuidv4 } from "uuid"
 
 const variableAction: { [variable: string]: (p: string) => string } = {
-  "{{:guid}}": generateGuid,
+  "{{:guid": generateGuid,
+  "{{:date": generateDate,
   // TODO:
-  // :date now
   // :rnd min max - random between
 }
 
@@ -12,12 +12,30 @@ export const dynamicVariablesParser = (data: string) => {
   for (let [key, value] of Object.entries(variableAction)) {
     let variableOccurencesCount = result.match(new RegExp(key, "g"))?.length ?? 0
     for (let i = 0; i < variableOccurencesCount; i++) {
-      result = result.replace(key, value(key))
+      let keyLength = key.length
+      let variablePosition = result.indexOf(key)
+      let variableEndPosition = result.indexOf("}}", variablePosition + 1)
+      let params = result.substring(variablePosition + keyLength + 1, variableEndPosition)
+
+      // Remove closing braces
+      result = result.substring(0, variablePosition + keyLength) + result.substring(variableEndPosition + 2);
+      result = result.replace(key, value(params))
     }
   }
+
+  console.log(result)
   return result
 }
 
 function generateGuid(input: string) {
   return uuidv4()
+}
+
+function generateDate(input: string) {
+  switch (input) {
+    case "now":
+      return (new Date()).toLocaleDateString();
+  }
+
+  return ""
 }
