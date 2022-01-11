a8a4667c8ac28a299518e607ad276ce4df8dce02
Seba G <sg@hetacode.pl>
2022-01-11 22:53:33 +0100

    Random generator variable

--- app/behaviour/dynamicVariables.ts
+++ app/behaviour/dynamicVariables.ts
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from "uuid"
 
-const variableAction: { [variable: string]: (p: string) => string } = {
+const variableAction: { [variable: string]: (p: string) => [string, boolean] } = {
   "{{:guid": generateGuid,
+  // date now
   "{{:date": generateDate,
-  // TODO:
-  // :rnd min max - random between
+  // rnd 10
+  // rnd 5 10
+  "{{:rnd": generateRandom,
 }
 
 export const dynamicVariablesParser = (data: string) => {
@@ -14,12 +16,18 @@ export const dynamicVariablesParser = (data: string) => {
     for (let i = 0; i < variableOccurencesCount; i++) {
       let keyLength = key.length
       let variablePosition = result.indexOf(key)
-      let variableEndPosition = result.indexOf("}}", variablePosition + 1)
-      let params = result.substring(variablePosition + keyLength + 1, variableEndPosition)
+      let variableEndPosition = result.indexOf("}}", variablePosition)
+      if (variableEndPosition < 0) {
+        continue
+      }
+      let params = result.substring(variablePosition + keyLength, variableEndPosition)
 
-      // Remove closing braces
-      result = result.substring(0, variablePosition + keyLength) + result.substring(variableEndPosition + 2);
-      result = result.replace(key, value(params))
+      let newValue = value(params.trim())
+      if (newValue[1]) {
+        // Remove closing braces
+        result = result.substring(0, variablePosition + keyLength) + result.substring(variableEndPosition + 2)
+        result = result.replace(key, newValue[0])
+      }
     }
   }
 
@@ -27,15 +35,34 @@ export const dynamicVariablesParser = (data: string) => {
   return result
 }
 
-function generateGuid(input: string) {
-  return uuidv4()
+function generateGuid(input: string): [string, boolean] {
+  return [uuidv4(), true]
 }
 
-function generateDate(input: string) {
+function generateDate(input: string): [string, boolean] {
   switch (input) {
     case "now":
-      return (new Date()).toLocaleDateString();
+      return [new Date().toISOString(), true]
   }
 
-  return ""
+  return ["", false]
+}
+
+function generateRandom(input: string): [string, boolean] {
+  if (!input) {
+    return ["", false]
+  }
+
+  let inputArr = input.split(" ")
+  let len = inputArr.length
+  switch (len) {
+    case 1:
+      return [`${Math.floor(Math.random() * Number.parseInt(inputArr[0]))}`, true]
+    case 2:
+      let min = Number.parseInt(inputArr[0])
+      let max = Number.parseInt(inputArr[1])
+      return [`${Math.floor(Math.random() * (max - min) + min)}`, true]
+    default:
+      return ["", false]
+  }
 }
